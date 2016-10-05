72b762089a93043a2d2433baa9d0bde7cfcad302
Pedro Reis <pdcreis@gmail.com>
2016-10-05 15:11:08 +0100

    Removed analytics code

--- header.php
+++ header.php
@@ -18,15 +18,6 @@
         <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
         <script src="//cdnjs.cloudflare.com/ajax/libs/selectivizr/1.0.2/selectivizr-min.js"></script>
         <![endif]-->
-        <script>
-            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-            ga('create', 'UA-85231448-1', 'auto');
-            ga('send', 'pageview');
-        </script>
     </head>
     <body <?php body_class(); ?>>
         <header class="header" role="banner">
